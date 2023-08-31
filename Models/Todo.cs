e651b368149e6782c2fdd15c8d41204c2fdb1763
Atta-123 <attaullahkhanafridi173@gmail.com>
2023-08-31 13:08:16 +0500

    hw

---
+++ Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ToDoApplication.Models
+{
+	public class Todo
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime DueDate { get; set; }
+		public string Priorities { get; set; }
+		public string Status { get; set; }
+	}
+}
